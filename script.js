4738d6fe6784c356ef827a54c070a9d2bd8a0096
Karthikeyan <karthigenius24@gmail.com>
2025-02-08 13:17:05 +0530

    Update

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Set the current year dynamically
+    AOS.init();
     document.getElementById('year').textContent = new Date().getFullYear();
+});
+
+particlesJS("particles-js", {
+    particles: {
+        number: { value: 100 },
+        size: { value: 3 },
+        move: { speed: 2 },
+        line_linked: { enable: true, distance: 150 }
+    }
 });
